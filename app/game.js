7ad6568eae3911039b73ce02c6dbbd2f93f39a1a
Robert Burek <robertburek@wp.pl>
2022-02-12 14:38:27 +0100

    Struktura klasy Game

--- app/game.js
+++ app/game.js
@@ -1,1 +1,9 @@
-console.log("witaj w diff");
+console.log("Jestem w DIFF !");
+class Game {
+    run(){
+        console.log("wystartowałem grę!!!");
+    }
+}
+
+const game = new Game();
+game.run();
