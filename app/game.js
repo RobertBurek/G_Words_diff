28a4d335ffd95779f0bd8a7fb976421be45d93e8
Robert Burek <robertburek@wp.pl>
2022-02-15 18:49:51 +0100

    Konstruktor oraz game.js - klawiatura
      będzie inne rozwiązanie z klawiaturą

--- app/game.js
+++ app/game.js
@@ -1,9 +1,25 @@
-console.log("Jestem w DIFF !");
 class Game {
-    run(){
-        console.log("wystartowałem grę!!!");
+    constructor({ lettersWrapper, categoryWrapper, wordGameWrapper }) {
+      this.lettersWrapper = lettersWrapper;
+      this.categoryWrapper = categoryWrapper;
+      this.wordGameWrapper = wordGameWrapper;
+      this.alphabet = [10, 11, 12, 13, 14, 15];
+      this.qwerty = [25, 28, 30, 42, 12];
     }
-}
-
-const game = new Game();
-game.run();
+    
+    writeLetter(letter) {
+        console.log(letter);
+    }
+  
+    run() {
+      console.log("wystartowałem grę!!!");
+      for (let i = 10; i < 36; i++) {
+        const letter = i.toString(36);
+        const button = document.createElement("button");
+        button.innerHTML = letter.toUpperCase();
+        button.addEventListener('click', () => this.writeLetter(letter));
+        this.lettersWrapper.appendChild(button);
+      }
+    }
+  }
+  
