c60e1ab7a55bea9c38b982c6dcf0666b1c8be6a4
RobertB <robertburek@wp.pl>
2022-02-15 22:12:42 +0100

    Dwie klawiatury
      alfabetyczna oraz qwerty

--- app/game.js
+++ app/game.js
@@ -1,25 +1,34 @@
 class Game {
-    constructor({ lettersWrapper, categoryWrapper, wordGameWrapper }) {
+    constructor({ lettersWrapper, lettersQWERTYWrapper, categoryWrapper, wordGameWrapper }) {
       this.lettersWrapper = lettersWrapper;
+      this.lettersQWERTYWrapper = lettersQWERTYWrapper;
       this.categoryWrapper = categoryWrapper;
       this.wordGameWrapper = wordGameWrapper;
-      this.alphabet = [10, 11, 12, 13, 14, 15];
-      this.qwerty = [25, 28, 30, 42, 12];
+      this.alphabet = [65, 260, 66, 67, 262,  68, 69, 280, 70, 71, 72, 73, 74, 75, 76, 321, 77, 78, 323, 79, 211,
+        80, 81, 82, 83, 346, 84, 85, 86, 87, 88, 89, 90, 377, 379];
+      this.qwerty = [81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 65, 83, 68, 70, 71, 72, 74, 75, 76, 90, 88, 67, 86,
+        66, 78, 77, 260, 262, 280, 321, 323, 211, 346, 377, 379];
     }
     
     writeLetter(letter) {
         console.log(letter);
     }
+
+    createKaybord(letters, wrapper) {
+      for (let i = 0; i < letters.length; i++) {
+        const letter = String.fromCharCode(letters[i]);
+        const button = document.createElement("button");
+        button.innerHTML = letter;
+        button.addEventListener('click', () => this.writeLetter(letter));
+        wrapper.appendChild(button);
+      }
+    }
   
     run() {
       console.log("wystartowałem grę!!!");
-      for (let i = 10; i < 36; i++) {
-        const letter = i.toString(36);
-        const button = document.createElement("button");
-        button.innerHTML = letter.toUpperCase();
-        button.addEventListener('click', () => this.writeLetter(letter));
-        this.lettersWrapper.appendChild(button);
-      }
+      this.createKaybord(this.alphabet, this.lettersWrapper);
+      this.createKaybord(this.qwerty, this.lettersQWERTYWrapper);
     }
+
   }
   
