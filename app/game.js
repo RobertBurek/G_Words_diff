da137972d5b0bb6a39387f79b0b8251c083da89b
RobertB <robertburek@wp.pl>
2022-02-17 19:51:38 +0100

    Klawiatura cd..

--- app/game.js
+++ app/game.js
@@ -1,18 +1,23 @@
 class Game {
-    constructor({ lettersWrapper, categoryWrapper, wordGameWrapper, keybourdScheme }) {
+    constructor({ lettersWrapper, categoryWrapper, wordGameWrapper, keybourdScheme, keybourd1, keybourd2, keybourd3, keybourd4 }) {
       this.lettersWrapper = lettersWrapper;
       this.categoryWrapper = categoryWrapper;
       this.wordGameWrapper = wordGameWrapper;
       this.keybourdScheme = keybourdScheme;
+      this.keybourd1 = keybourd1;
+      this.keybourd2 = keybourd2;
+      this.keybourd3 = keybourd3;
+      this.keybourd4 = keybourd4;
       this.alphabet = [65, 260, 66, 67, 262,  68, 69, 280, 70, 71, 72, 73, 74, 75, 76, 321, 77, 78, 323, 79, 211,
         80, 81, 82, 83, 346, 8678, 84, 85, 86, 87, 88, 89, 90, 377, 379];
       this.qwerty = [81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 65, 83, 68, 70, 71, 72, 74, 75, 76, 90, 88, 67, 86,
-        66, 78, 77, 260, 262, 8678, 280, 321, 323, 211, 346, 377, 379];
+        66, 78, 77, 8678, 260, 262, 280, 321, 323, 211, 346, 377, 379];
       this.currentlyKeybourd = this.alphabet;
     }
     
     writeLetter(letter) {
         console.log(letter.charCodeAt());
+        console.log(letter);
     }
 
     changeKeybourd() {
@@ -20,14 +25,38 @@ class Game {
         else this.currentlyKeybourd = this.alphabet;
     }
 
-     createKeyboard(lettersCheme, wrapper) {
-       wrapper.innerHTML = "";
-      for (let i = 0; i < lettersCheme.length; i++) {
+     createKeyboard(lettersCheme, keybourd1, keybourd2, keybourd3, keybourd4) {
+      keybourd1.innerHTML = "";
+      keybourd2.innerHTML = "";
+      keybourd3.innerHTML = "";
+      keybourd4.innerHTML = "";
+      for (let i = 0; i < 10; i++) {
         const letter = String.fromCharCode(lettersCheme[i]);
         const button = document.createElement("button");
         button.innerHTML = letter;
         button.addEventListener('click', () => this.writeLetter(letter));
-        wrapper.appendChild(button);
+        keybourd1.appendChild(button);
+      }
+      for (let i = 10; i < 19; i++) {
+        const letter = String.fromCharCode(lettersCheme[i]);
+        const button = document.createElement("button");
+        button.innerHTML = letter;
+        button.addEventListener('click', () => this.writeLetter(letter));
+        keybourd2.appendChild(button);
+      }
+      for (let i = 19; i < 27; i++) {
+        const letter = String.fromCharCode(lettersCheme[i]);
+        const button = document.createElement("button");
+        button.innerHTML = letter;
+        button.addEventListener('click', () => this.writeLetter(letter));
+        keybourd3.appendChild(button);
+      }
+      for (let i = 27; i < 36; i++) {
+        const letter = String.fromCharCode(lettersCheme[i]);
+        const button = document.createElement("button");
+        button.innerHTML = letter;
+        button.addEventListener('click', () => this.writeLetter(letter));
+        keybourd4.appendChild(button);
       }
     }
 
@@ -35,6 +64,7 @@ class Game {
       wrapper.innerHTML = "";
      for (let i = 8650; i < 8890; i++) {
        const letter = String.fromCharCode(i);
+      //  8674  8801  8811 8688
        const button = document.createElement("button");
        button.innerHTML = letter;
        button.addEventListener('click', () => this.writeLetter(letter));
@@ -43,12 +73,11 @@ class Game {
    }
   
     run() {
-      this.createKeyboard(this.currentlyKeybourd, this.lettersWrapper);
+      this.createKeyboard(this.currentlyKeybourd, this.keybourd1, this.keybourd2, this.keybourd3, this.keybourd4);
       this.keybourdScheme.addEventListener('click', () =>{
           this.changeKeybourd();
-          this.createKeyboard(this.currentlyKeybourd, this.lettersWrapper);
+          this.createKeyboard(this.currentlyKeybourd, this.keybourd1, this.keybourd2, this.keybourd3, this.keybourd4);
       })
-      console.log("wystartowałem grę!!!");
       // this.charsListing(this.lettersWrapper);
     }
 
