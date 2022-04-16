4bc7f362ed6fb682b1f6dc4deb41bc277cd7b4c8
RobertB <robertburek@wp.pl>
2022-04-16 19:38:32 +0200

    cd..

--- php/logout.php
+++ php/logout.php
@@ -1,16 +1,7 @@
 <?php
 
 	session_start();
-		if (isset($_GET['strona']))  
-		{	$nazwStr=$_GET['strona']; 
-			$_SESSION['nazwapliku']=$nazwStr; 
-		} else 
-				{ 
-					if (isset($_SESSION['nazwapliku'])) $nazwStr=$_SESSION['nazwapliku']; else $nazwStr="Index";
-				}
-	
-	$nazwapliku="index.php?strona=".$_SESSION['nazwapliku'];
 	session_unset();
 //	header('Location:index.php');
-	header('Location:'.$nazwapliku);
+	header('Location:index.php');
 ?>
