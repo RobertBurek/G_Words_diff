d339244f00a43fda88114969fbd26221e89ccdf6
RobertB <robertburek@wp.pl>
2022-07-10 21:47:15 +0200

    Refaktor connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,10 +1,5 @@
 <?php
 
-	// $host="192.168.101.60";
-	// $db_user="Robert";
-	// $db_password="Wordle";
-	// $db_name="jakie_to_slowo";
-
     // $host="127.0.0.1";
 	// $db_user="Robert";
 	// $db_password="Wordle";
@@ -14,5 +9,3 @@
 	$db_user="robbur70_AdminWordle";
 	$db_password="Wordle56789";
 	$db_name="robbur70_jakie_to_slowo";
-
-?>
