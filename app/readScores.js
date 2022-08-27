e1ff8c74c2b226639ab85c1d03c6930ac7e294cd
RobertB <robertburek@wp.pl>
2022-08-27 22:34:20 +0200

    Refaktor autoLogin.js

--- app/readScores.js
+++ app/readScores.js
@@ -1,21 +1,19 @@
-
-
 $.post(
-    "./php/readResultsPlayerWithBaseSQL.php",
-    { nameTable: localStorage.getItem('nameTable/JTS')},
-    function (dataSQL) {
-      console.log('jestem w readScores.js');
-      console.log(dataSQL);
-      localStorage.setItem("result5/JTS", dataSQL.result5);
-      localStorage.setItem("result6/JTS", dataSQL.result6);
-      localStorage.setItem("result7/JTS", dataSQL.result7);
-      localStorage.setItem("result8/JTS", dataSQL.result8);
-      localStorage.setItem("result9/JTS", dataSQL.result9);
-      $.getScript("app/displayScores.js").done(function () {
-        console.log("Wyświetlanie wyników gracza  displayScores.js");
-    });
-    },
-    "json"
-  ).fail(function (data) {
-    console.log('nie udało się odczyta wyników gracza');
-  });
+	"./php/readResultsPlayerWithBaseSQL.php",
+	{ nameTable: localStorage.getItem("nameTable/JTS") },
+	function (dataSQL) {
+		console.log("jestem w readScores.js");
+		console.log(dataSQL);
+		localStorage.setItem("result5/JTS", dataSQL.result5);
+		localStorage.setItem("result6/JTS", dataSQL.result6);
+		localStorage.setItem("result7/JTS", dataSQL.result7);
+		localStorage.setItem("result8/JTS", dataSQL.result8);
+		localStorage.setItem("result9/JTS", dataSQL.result9);
+		$.getScript("app/displayScores.js").done(function () {
+			console.log("Wyświetlanie wyników gracza - displayScores.js");
+		});
+	},
+	"json"
+).fail(function () {
+	console.log("Nie udało się odczyta wyników gracza.");
+});
