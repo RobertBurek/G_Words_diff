276cbbf7bec9afbf7211816a4bdd77baa61e9a13
RobertB <robertburek@wp.pl>
2022-08-27 14:08:13 +0200

    Odczyt wyników gracza cd..

--- app/readScores.js
+++ app/readScores.js
@@ -4,6 +4,11 @@ $.post(
     function (dataSQL) {
       console.log('jestem w readScores.js');
       console.log(dataSQL);
+      localStorage.setItem("result5/JTS", dataSQL.result5);
+      localStorage.setItem("result6/JTS", dataSQL.result6);
+      localStorage.setItem("result7/JTS", dataSQL.result7);
+      localStorage.setItem("result8/JTS", dataSQL.result8);
+      localStorage.setItem("result9/JTS", dataSQL.result9);
     },
     "json"
   ).fail(function (data) {
