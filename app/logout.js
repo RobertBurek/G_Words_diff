8bb995c05be660ff27ff4e037cda4e37a4d184b0
RobertB <robertburek@wp.pl>
2022-08-28 21:41:13 +0200

    Refaktor logout.js

--- app/logout.js
+++ app/logout.js
@@ -1,30 +1,24 @@
 const logoutBtn = document.querySelector(".logout-btn");
-// const inputNick = document.querySelector("[name='nick']");
-// console.log(registerBtn);
 const loggingButton = document.getElementById("logging");
 const loggingDivInfo = document.querySelector(".logging").parentNode;
-
-
 
 try {
 	logoutBtn.addEventListener("click", () => {
 		localStorage.setItem("nick/JTS", "");
-		// localStorage.setItem("nameTable/JTS", "");
-		// localStorage.setItem("result5/JTS", 0);
-		// localStorage.setItem("result6/JTS", 0);
-		// localStorage.setItem("result7/JTS", 0);
-		// localStorage.setItem("result8/JTS", 0);
-		// localStorage.setItem("result9/JTS", 0);
-		console.log("Wyczysciłem localStorage !!!");
+		localStorage.setItem("nameTable/JTS", "");
+		localStorage.setItem("result5/JTS", 0);
+		localStorage.setItem("result6/JTS", 0);
+		localStorage.setItem("result7/JTS", 0);
+		localStorage.setItem("result8/JTS", 0);
+		localStorage.setItem("result9/JTS", 0);
+		console.log("Wyczyszczono dane !!!");
 
-		loggingButton.innerHTML =
-		`
+		loggingButton.innerHTML = `
 			<i class="fas fa-sign-in-alt" dropdown></i>
 			Logowanie <div class="dropdown-note" dropdown>(rejestracja)</div>
 		`;
 
-		loggingDivInfo.innerHTML = 
-			`<div class="logging" logging>
+		loggingDivInfo.innerHTML = `<div class="logging" logging>
 				<label class="dropdown-login" logging>Nazwa (login):
 				    <input type="text" name="nick" placeholder="" logging></label>
 				<label class="dropdown-password" logging>Hasło:
@@ -35,71 +29,15 @@ try {
 				</div>
 			</div>`;
 
-		$.getScript("app/login.js")
-		.done(function () {
+		$.getScript("app/login.js").done(function () {
 			console.log("inicjacja login.js");
-		// })
-		// .fail(function () {
-		// 	console.log("coś poszło nie tak w autoLogin");
 		});
-		$.getScript("app/register.js")
-		.done(function () {
+		$.getScript("app/register.js").done(function () {
 			console.log("inicjacja register.js");
-		// })
-		// .fail(function () {
-		// 	console.log("coś poszło nie tak w autoLogin");
 		});
 	});
 } catch (e) {
 	if (e instanceof ReferenceError) {
-		console.log("logoutBtn - is not defined");
+		console.log("logoutBtn - nie jest zdefiniowany.");
 	}
 }
-
-// -------------------------------------------------------------
-
-// try {
-// 	// typeof registerBtn === "undefined";
-// 	logoutBtn.addEventListener("click", () => {
-// 		// console.log(loginBtn);
-// 		// console.log(inputNick.value);
-// 		// console.log(inputPassword.value);
-// 		const dataLogin = { Nick: '' };
-// 		$.post(
-// 			"./php/logout.php",
-// 			dataLogin,
-// 			function (data) {
-// 				// alert( "OK - odczyt / zapis do bazy" );
-// 				// console.log(data.name);
-// 				console.log("Wylogowano: " + data.nick);
-// 				localStorage.setItem('nick/JTS','');
-// 			},
-// 			"json"
-// 		).fail(function () {
-// 			alert("Błąd reakcji z logout.php");
-// 		});
-// 	});
-// } catch (e) {
-// 	if (e instanceof ReferenceError) {
-// 		console.log("logoutBtn - is not defined");
-// 	}
-// }
-
-// --------------------------------------------------------------
-
-// registerBtn.addEventListener('click', ()=>{
-//     // console.log(loginBtn);
-//     // console.log(inputNick.value);
-//     // console.log(inputPassword.value);
-//     const dataLogin = {Nick: inputNick.value}
-//     $.post( "./php/register.php", dataLogin
-//     , function(data) {
-//         // alert( "OK - odczyt / zapis do bazy" );
-//         // console.log(data.name);
-//         console.log('Wylogowano: ' + data.nick);
-//     }, "json")
-//     .fail(function() {
-//         alert( "Błąd reakcji z register.php" );
-//     }
-// );
-// })
