9255837e5d2f34e6d127910bff144ecd6c639eb0
RobertB <robertburek@wp.pl>
2022-09-25 21:35:30 +0200

    Dodatkowa baza fanych na OVH

--- php/connect.php
+++ php/connect.php
@@ -1,11 +1,17 @@
 <?php
 
-    $host="127.0.0.1";
-	$db_user="Robert";
-	$db_password="Wordle";
-	$db_name="jakie_to_slowo";
+    // $host="127.0.0.1";
+	// $db_user="Robert";
+	// $db_password="Wordle";
+	// $db_name="jakie_to_slowo";
 
 	// $host="192.168.101.60";
 	// $db_user="robbur70_AdminWordle";
 	// $db_password="Wordle56789";
 	// $db_name="robbur70_jakie_to_slowo";
+
+
+	$host="robertkierownik.mysql.db";
+	$db_user="robertkierownik";
+	$db_password="RobertKierownik123";
+	$db_name="robertkierownik";
